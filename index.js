e354b94dbb2d9ca723edbdafc5013c1ca3db549f
sjrich <steven.richards.softco@gmail.com>
2016-11-30 07:32:41 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var pgp = require('pg-promise')
+
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
+
+//do database stuff
+var pg = require('pg');
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM COMPANY', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log('***DATABASE-RESULT***');
+    console.log(result.rows);
+  });
+});
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
